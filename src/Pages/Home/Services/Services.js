d7a5be4da8bae72ac96930eafb9d6325072c169a
Anwar Sagor <sagor.onnet@gmail.com>
2022-05-23 12:25:01 +0600

    Dynamic Page Title

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -18,10 +18,11 @@ const Services = () => {
             <h1 className='services-title'>Our Services</h1>
             <div className='services-container'>
                 {
-                    services.map(service => <Service
-                        key={service.id}
-                        service={service}
-                    ></Service>)
+                    services.map(service =>
+                        <Service
+                            key={service.id}
+                            service={service}
+                        ></Service>)
                 }
             </div>
         </div>
